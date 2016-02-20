27f594bc3c82b89962988d9f98d8f7e28353b09b
Wiley Walker <wiley-walker@live.com>
2016-02-20 10:39:20 -0800

    finished part 1

--- csharp/Cheers/Cheers/Program.cs
+++ csharp/Cheers/Cheers/Program.cs
@@ -9,7 +9,23 @@ namespace Cheers
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            string name = "";
+            Console.WriteLine("Please input your name");
+            
+            string x = Console.ReadLine();
+            name = x;
+
+            int times = name.Length;
+            char[] letters = name.ToCharArray();
+             
+            for (int i = 0; i < times; i++)
+            {
+                Console.Write("Give me a " + letters[i] + "\r\n");
+                
+            }
+            Console.WriteLine( "Hooray " + name + "!!!!");
+            Console.ReadLine();
         }
     }
 }
